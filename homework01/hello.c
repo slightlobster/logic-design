63f7e3082e9757c71e798a8b10f5ddc2a6447b3f
Zachary Kennedy <zkennedy@student11.cse.nd.edu>
2023-08-28 20:48:28 -0400

    HW1: Part 4

--- homework01/hello.c
+++ homework01/hello.c
@@ -2,6 +2,7 @@
 
 int main() {
 
+  // start of part 2
   printf("Hello, world!\n");
 
   int x = 2;
@@ -16,6 +17,12 @@ int main() {
   printf("Decimal: %d, Hex: %x, ASCII: %c\n", x,x,x);
 
   x = 'z';
-  printf("Decimal: %d, Hex: %x, ASCII: %c\n", x,x,x);
+  printf("Decimal: %d, Hex: %x, ASCII: %c\n\n", x,x,x);
+
+  // start of part 3
+  printf("-1: %x, -2: %x, -3: %x\n", -1, -2, -3);
+
+  printf("0x7ffffffe: %d, 0x7fffffff: %d\n", 0x7ffffffe, 0x7fffffff);
+  printf("0x80000000: %d, 0x80000001: %d\n\n", 0x80000000, 0x80000001);
   return 0;
 }
