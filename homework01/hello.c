8a211a1d45931e6d9e53412bc33ffd0fa5d06058
Zachary Kennedy <zkennedy@student11.cse.nd.edu>
2023-08-28 21:42:55 -0400

    HW1: All parts

--- homework01/hello.c
+++ homework01/hello.c
@@ -26,11 +26,39 @@ int main() {
   printf("0x80000000: %d, 0x80000001: %d\n\n", 0x80000000, 0x80000001);
 
   // start of part 4
-  int unsigned_x = 0xffffffff;
-  printf("0xffffffff as unsigned int: %u\n\n", unsigned_x);
+  int c = 0xffffffff;
+  printf("0xffffffff as unsigned int: %u\n\n", c);
 
   // start of part 5
-   
+  int a = 0x3;
+  int b = 0x5;
+  printf("a = %x", a);
+  printf("a & b = %x\n", a & b);
+  printf("not a = %x\n", ~a);
+  printf("a | b = %x\n", a | b);
+
+  // and/or masking
+  printf("a & 0x0000000f = %x\n", a & 0x0000000f); 
+  printf("a & 0xfffffff0 = %x\n\n", a | 0xfffffff0);
+ 
+  //start of part 6
+  int z = 12;
+  
+  //left shift
+  printf("int 12 << 1 = 0x%x = 0d%d\n", z << 1, z << 1);
+
+  //right shift
+  printf("int 12 >> 1 = 0x%x = 0d%d\n", z >> 1, z >> 1);
+  z = -12;
+  printf("int -12 >> 1 = 0x%x = 0d%d\n", z >> 1, z >> 1);
+
+  //using unsigned ints
+  unsigned int n = 12;
+  printf("unsigned int 12 << 1 = 0x%x = 0d%u\n", n << 1, n << 1);
+  printf("unsigned int 12 >> 1 = 0x%x = 0d%u\n", n >> 1, n >> 1);
+  n = -12;
+  printf("unsigned int -12 >> 1 = 0x%x = 0d%u\n", n >> 1, n << 1);
+  
 
   return 0;
 }
